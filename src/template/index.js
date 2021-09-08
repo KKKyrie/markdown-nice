6cca5917e286b2f7efec26fd36f4f63a1456d071
kyrieliu(刘佳宁) <kyrieliu@webank.com>
2021-09-08 15:04:07 +0800

    fix(template): fix initial content with string var

--- src/template/index.js
+++ src/template/index.js
@@ -1,8 +1,6 @@
 import custom from "./markdown/custom";
 import normal from "./markdown/normal";
 
-import content from "./content.md";
-
 import atomOneDark from "./code/atomOneDark";
 import atomOneLight from "./code/atomOneLight";
 import github from "./code/github";
@@ -18,6 +16,20 @@ import macVs2015 from "./macCode/macVs2015";
 import macXcode from "./macCode/macXcode";
 
 import basic from "./basic";
+
+const content = `## 来了老弟？  
+> 本编辑器是基于 [mdnice](https://github.com/mdnice/markdown-nice) 的开源版本打造。  
+  
+在这里，写 markdown 就好了。  
+更多功能，可查看“菜单-教程”  
+  
+另外，主题可以自定义，点击“菜单-主题”即可，实时编辑、实时生效、实时保存。   
+  
+若有其他问题，可添加我的微信${`K-I2ving`}聊聊。  
+<img
+    src="https://kyrieliu.cn/images/qrcode2.jpg"
+    width="200px"
+/>`;
 
 export default {
   basic,
